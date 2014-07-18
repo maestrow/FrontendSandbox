314d91b9c32a0e284098b5702107a124c696ee73
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-07-19 03:05:06 +0400

    -

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var static = require('koa-static');
 var route = require('koa-route');
 var fs = require('fs');
 var interpolate = require('interpolate');
+var ejs = require('ejs');
 
 
 // Creating app & Setup
@@ -11,6 +12,8 @@ var interpolate = require('interpolate');
 var isDebug = false;
 var app = koa();
 var render = views(__dirname + '/views', { ext: 'ejs' });
+var overridingMasterView = __dirname + '/../../views/master.ejs';
+var overrideMasterView = fs.existsSync(overridingMasterView);
 
 
 // Routing
@@ -39,6 +42,14 @@ var getScripts = function (moduleName) {
   return allFiles.filter(filter).map(map);
 };
 
+var getMasterViewData = function (moduleName) {
+  return {
+    title: moduleName,
+    scripts: getScripts(moduleName),
+    body: fs.readFileSync(getLayoutFileName(moduleName), {encoding:'utf8'})
+  };
+};
+
 
 // Controllers
 
@@ -49,11 +60,11 @@ function *list() {
 }
 
 function *show(moduleName) {
-  this.body = yield render('master', {
-    title: moduleName,
-    scripts: getScripts(moduleName),
-    body: fs.readFileSync(getLayoutFileName(moduleName), {encoding:'utf8'})
-  })
+  if (!overrideMasterView)
+    this.body = yield render('master', getMasterViewData(moduleName))
+  else {
+    this.body = ejs.render(fs.readFileSync(overridingMasterView, {encoding:'utf8'}), getMasterViewData(moduleName));
+  }
 }
 
 app.listen(4000);
